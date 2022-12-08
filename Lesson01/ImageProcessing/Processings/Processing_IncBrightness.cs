82ba42768d8db2a664d59a7278d300e738115497
Teacher Peng <pengsw@mail.buct.edu.cn>
2022-12-08 21:40:12 +0800

    测试

--- Lesson01/ImageProcessing/Processings/Processing_IncBrightness.cs
+++ Lesson01/ImageProcessing/Processings/Processing_IncBrightness.cs
@@ -8,12 +8,11 @@ namespace ImageProcessing.Processings
         public Processing_IncBrightness()
         {
             _Control = new Ui_Slider(this);
-            Level = 50;
         }
         public override string Name { get { return "Brightness"; } }
         public override UserControl Control { get { return _Control; } }
         private Ui_Slider _Control;
-        public double Level { get; set; }
+        public double Level { get; set; } = 60;
         
         protected override byte[] ProcessImage(byte[] aSourceRawData, ref int aPixelWidth, ref int aPixelHeight, int aBytesPerPixel, ref int aStride)
         {
